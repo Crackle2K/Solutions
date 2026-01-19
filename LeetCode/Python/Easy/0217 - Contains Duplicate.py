e1f0cedb4cdc536f23b17da27b61758c26934858
Dinesh Sinnathamby <103861462+Crackle2K@users.noreply.github.com>
2026-01-18 19:22:18 -0500

    Implement containsDuplicate function in Solution class

---
+++ LeetCode/Python/Easy/0217 - Contains Duplicate.py
@@ -0,0 +1,8 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        checkSet = set()
+        for num in nums:
+            if num in checkSet:
+                return True
+            checkSet.add(num)
+        return False
