c6164475d1e562d135f513afe0c6a96ee1457204
Dinesh Sinnathamby <103861462+Crackle2K@users.noreply.github.com>
2026-02-04 19:01:43 -0500

    Add CCC '12 J3 - Icon Scaling.py for icon scaling

---
+++ DMOJ/CCC/Python/CCC '12 J3 - Icon Scaling.py
@@ -0,0 +1,12 @@
+
+"""
+*x*
+ xx
+* *
+"""
+
+k = int(input())
+
+for i in range(k): print(('*' * k) + ('x' * k) + ('*' * k))
+for i in range(k): print((' ' * k) + ('x' * k) + ('x' * k))
+for i in range(k): print(('*' * k) + (' ' * k) + ('*' * k))
