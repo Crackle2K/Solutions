b470a3de9d3eb4d4b53b497a8d4645c900cbe620
Dinesh Sinnathamby <103861462+Crackle2K@users.noreply.github.com>
2026-02-18 16:45:26 -0500

    Create DMOPC '21 Contest 10 P5 - Number Theory.py
    Add initial implementation for DMOPC '21 Contest 10 P5.

---
+++ DMOJ/DMOPC/Python/DMOPC '21 Contest 10 P5 - Number Theory.py
@@ -0,0 +1,9 @@
+"""
+- the number is going to be a multiple of 3
+- I only need to trial and error it till it works
+"""
+
+print(15230439315)
+# I swear this question is built to catch people using AI or something cause how are you meant to do this
+# Ohhh okay so this question is a troll
+# wp
